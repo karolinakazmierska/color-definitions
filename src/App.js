c01bfedc43046ea56f9e6a55cb455b0557f6a33a
karolinakazmierska <karo.a.kazmierska@gmail.com>
2022-01-12 22:38:27 +0100

    Add component to view, add and delete color definitions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,116 @@
 import './App.css';
+import React, { useState, useEffect} from 'react';
 
 function App() {
+    const [colors, setColors] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [colorsCount, setColorsCount] = useState(0);
+
+    const initialHex = "#b866d2";
+
+    const [newColorName, setNewColorName] = useState("");
+    const [newColorHex, setNewColorHex] = useState(initialHex);
+    const [addNewColorError, setAddNewColorError] = useState(false);
+
+    useEffect(() => {
+        fetch('https://colors-definitions-api.herokuapp.com/colors')
+            .then(response => response.json())
+            .then(data => {
+                setColors(data);
+                setColorsCount(data.length);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setError(true);
+            })
+    }, [colorsCount])
+
+    const addColor = (e) => {
+        e.preventDefault();
+        if (!newColorName || !newColorHex) {
+            setAddNewColorError(true);
+            return;
+        }
+
+        fetch('https://colors-definitions-api.herokuapp.com/colors', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: newColorName, hex: newColorHex })
+            })
+            .then(response => response.json())
+            .then(data => {
+                setAddNewColorError(false);
+                setNewColorName("");
+                setNewColorHex(initialHex);
+                setColorsCount(colorsCount + 1);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+     };
+
+     const deleteColor = (id) => {
+        fetch(`https://colors-definitions-api.herokuapp.com/colors/${id}`, {
+                method: 'DELETE'
+            })
+            .then(response => response.json())
+            .then(data => {
+                setColorsCount(colorsCount - 1);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+     }
+
+    if (loading) return "Loading...";
+    if (error) return "Error";
+
     return (
-        <div className="App">
-            Hello
+        <div className="container">
+            <div className="title">Color definitions app</div>
+            <div className="add-color-container">
+                <form onSubmit={addColor}>
+                    <div className="input-container">
+                        <label for="newColorName">Name:</label>
+                        <input
+                            name="newColorName"
+                            type="text"
+                            value={newColorName}
+                            onChange={e => setNewColorName(e.target.value)}
+                        />
+                    </div>
+                    <div className="input-container">
+                        <label for="newColorHex">Hex:</label>
+                        <input
+                            name="newColorHex"
+                            type="color"
+                            value={newColorHex}
+                            onChange={e => setNewColorHex(e.target.value)}
+                        />
+                    </div>
+                    <input type="submit" value="Submit" className="submit" />
+                    {
+                        addNewColorError && <span className="error">Fill in all fields</span>
+                    }
+                </form>
+            </div>
+            <div className="colors-container">
+                {colors.map(color => {
+                    return (
+                        <div className="color-container" key={color.id}>
+                            <div className="color-box" style={{backgroundColor: color.hex}}></div>
+                            <p>{color.name}</p>
+                            <p>{color.hex}</p>
+                            <div className="delete" onClick={() => deleteColor(color.id)}>delete</div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
